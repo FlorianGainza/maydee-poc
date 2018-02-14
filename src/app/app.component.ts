aa825fa162e8fa5ab9f3b4d06c23b1a016a9b6b1
Florian Gainza <florian@Florian-PC.local>
2018-02-14 23:54:54 +0100

    Add assets paths to first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  maydeeImagePath:   string;
+  twitterImagePath:  string;
+  facebookImagePath: string;
+  googleImagePath:   string;
+  linkedinImagePath: string
+
+  constructor() {
+    this.maydeeImagePath   = 'assets/img/Maydée-logo-entier-e1511168387551.png';
+    this.twitterImagePath  = 'assets/img/if_twitter_1279054.png';
+    this.facebookImagePath = 'assets/img/if_facebook_1279052.png';
+    this.googleImagePath   = 'assets/img/if_google-plus_1279046.png';
+    this.linkedinImagePath = 'assets/img/if_linkedin_1279057.png';
+  }
 }
